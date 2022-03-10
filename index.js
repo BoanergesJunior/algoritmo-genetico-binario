189c7d7e309ddf4ee6a63aa364bf5b054ad1f34a
BoanergesJunior <boanerges_junior@hotmail.com>
2022-03-10 15:02:00 -0300

    tournamentSelection and probability done

--- index.js
+++ index.js
@@ -5,20 +5,64 @@ const objectiveFunction = (x) => {
   return Math.pow(x, 2) - (3 * x) + 4
 }
 
+const getProbability = (population) => {
+  const sumFx = population.reduce((acc, _, index) => {
+    return acc + population[index].objectiveFunctionValue
+  }, 0)
+
+  const populationWithProbability = population.map((specimen) => {
+    return {
+      ...specimen,
+      probability: specimen.objectiveFunctionValue / sumFx
+    }
+  })
+
+  return populationWithProbability
+}
+
+const tournamentSelection = (population) => {
+  const firstChosen = population[Math.floor(Math.random() * population.length)]
+  const secondChosen = population[Math.floor(Math.random() * population.length)]
+
+  return firstChosen.objectiveFunctionValue > secondChosen.objectiveFunctionValue ? firstChosen : secondChosen
+}
+
 const initialPopulation = () => {
   const population = [];
   for (let i = 0; i < numberOfInitialPopulation; i++) {
     let number = Math.random() * (10 - (-10)) + (-10);
     number = Math.ceil(number)
-    console.log(number)
     if (number < 0) {
       number = number * -1
-      population.push('1' + number.toString(2).padStart(4, '0'))
+      population.push(
+        {
+          decimalSpecimen: number,
+          binarySpecimen: '1' + number.toString(2).padStart(4, '0'),
+          objectiveFunctionValue: objectiveFunction(number)
+        }
+      )
     } else {
-      population.push('0' + number.toString(2).padStart(4, '0'))
+      population.push(
+        {
+          decimalSpecimen: number,
+          binarySpecimen: '0' + number.toString(2).padStart(4, '0'),
+          objectiveFunctionValue: objectiveFunction(number)
+        }
+      )
     }
   }
   return population;
 }
 
-console.log(objectiveFunction(-4.3))
+function init() {
+  const population = initialPopulation()
+  const populationWithProbability = getProbability(population)
+
+  const firstChosen = tournamentSelection(populationWithProbability)
+  const populationWithoutFirst = populationWithProbability.filter((element) => element !== firstChosen)
+  const secondChosen = tournamentSelection(populationWithoutFirst)
+
+  console.log(firstChosen)
+  console.log(secondChosen)
+}
+init()
